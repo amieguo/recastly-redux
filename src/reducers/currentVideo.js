e23d85bc59aea9840f62cb245a44932ae5ef711a
amieguo <amieguo@gmail.com>
2018-10-03 18:26:35 -0700

    got all but last root reducer

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,19 @@
 import Redux from 'redux';
+import changeVideo from '../actions/currentVideo.js';
+// import {CHANGE_VIDEO} from '../actions/currentVideo.js';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  if (action.type === 'CHANGE_VIDEO') {
+    // console.log('2ndlog:', Object.assign({}, state, {currentVideo: action.video}))
+    return Object.assign({}, state, action.video)
+    // return action.video;
+  }
+  
+  
+  
+  
+  return state
 };
 
 export default currentVideoReducer;
